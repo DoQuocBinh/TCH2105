d666f5f578e42b87256c91dbbf74b47e0ac8fa8d
DoQuocBinh <binhdq@fpt.edu.vn>
2022-08-02 15:22:03 +0700

    ok

---
+++ EntryApp/app/src/main/java/fpt/grw/entryapp/MainActivity.java
@@ -0,0 +1,36 @@
+package fpt.grw.entryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOk =  findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sayHi();
+            }
+        });
+    }
+
+    private void sayHi() {
+        //refer to the textField
+        EditText txtName =  findViewById(R.id.txtName);
+        //get the user's input
+        String name = txtName.getText().toString();
+        //display to user via Toast
+        Toast.makeText(getApplicationContext(),"Hello " + name,Toast.LENGTH_LONG).show();
+    }
+
+}
