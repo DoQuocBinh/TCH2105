3ea7cc55442297d900397c493ccfc177d4a84b7c
DoQuocBinh <binhdq@fpt.edu.vn>
2022-09-20 14:27:36 +0700

    ok

--- ReviewAndroid2/app/src/main/java/fpt/grw/reviewandroid/ExamDetails.java
+++ ReviewAndroid2/app/src/main/java/fpt/grw/reviewandroid/ExamDetails.java
@@ -2,11 +2,16 @@ package fpt.grw.reviewandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ExamDetails extends AppCompatActivity {
+    ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,16 @@ public class ExamDetails extends AppCompatActivity {
         String date = intent.getStringExtra("exam_date");
 
         EditText inputName = findViewById(R.id.inputNameDetail);
-        EditText inputDate = findViewById(R.id.inputDateDetail);
+        EditText inputDate = findViewById(R.id.inputQuestion);
         inputName.setText(name);
         inputDate.setText(date);
+
+        Button btnPrev = findViewById(R.id.btnPrev);
+        btnPrev.setOnClickListener(view -> {
+            TextView inputPicURL = findViewById(R.id.inputPictureURL);
+            ImageView imageView = findViewById(R.id.imageView);
+            DownloadImageTask task = new DownloadImageTask(mProgressDialog,this,imageView);
+            task.execute(inputPicURL.getText().toString());
+        });
     }
 }
